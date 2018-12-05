bdec256aaff75049b9a66aae0fc757e80750edae
Alex Waweru <njorogealexw@gmail.com>
2018-12-04 17:11:24 -0800

    cleaned the training data

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # group_project
+
+## Cleaned data
+- Removed all the line numbering 
+
+Located at ./training_data/clean_data
+
+- If you still want to use the original data
+
+Located at ./training_data/original_data
---
+++ topics_classification/k-nearest-neighbour/main.py
@@ -0,0 +1,6 @@
+
+# The files containing clean training data
+questions_file = "../../training_data/cleaned_data/questions.txt"
+answers_file = "../../training_data/cleaned_data/Answers.txt"
+topics_file = "../../training_data/cleaned_data/Topics.txt"
+
