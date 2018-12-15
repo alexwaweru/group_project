4cdfe08047369eef52cce156cec2a64d435315e0
Alex Njoroge Waweru <njorogealexw@gmail.com>
2018-12-14 18:07:05 -0800

    implemented jaccard similarity

--- topics_classification/k-nearest-neighbour/main.py
+++ topics_classification/k-nearest-neighbour/main.py
@@ -12,7 +12,7 @@ def k_nearest_neighbour(document, k, questions_filename, topics_filename):
     """ Takes a document, a integer k, questions filename and topics filename as inputs.
     Returns a topic which the document belongs to.
     """
-    N, documents, matrix = create_word_vector(questions_filename)
+    N, documents, topics, matrix = create_word_vector(questions_filename, topics_filename)
     similarities = {}
     """ This section calculates the similarity index between the test doc and each doc in the training set"""
     a = tf_idf(document, matrix, N)     #Generate a tf-idf vector for the test document
@@ -30,21 +30,8 @@ def k_nearest_neighbour(document, k, questions_filename, topics_filename):
         k_nearest_neighbours.append(item[0])
     """ This section gets the topics that the k-nearest questions belong to"""
     k_nearest_topics = []
-    questions = []
-    topics = []
-    try:
-        questions_file = open(questions_filename, 'r')
-        topics_file = open(topics_filename, 'r')
-        for query in (questions_file.readlines()):
-            questions.append(query)
-        for topic in (topics_file.readlines()):
-            topics.append(topic)
-        questions_file.close()
-        topics_file.close()
-    except IOError:
-        logging.exception('')
-    for i in range(len(questions)):
-        if questions[i] in  k_nearest_neighbours:
+    for i in range(len(documents)):
+        if documents[i] in  k_nearest_neighbours:
             k_nearest_topics.append(topics[i])
     k_nearest_topics = Counter(k_nearest_topics)
     return k_nearest_topics.most_common(1)[0][0]    #Return the topic that is most common among the k neighbors
@@ -63,28 +50,28 @@ def tf_idf(document, matrix, N):
         for mark in noise:
             word = word.replace(mark,'')
     """ This section calculates the tf-idf of each term and create a vector of the tf-idfs"""
-    unique_words = list(set(matrix.elements()))     #Creates a list of uniques words 
     for i in range(len(unique_words)):
         if unique_words[i] in document:
             tf = (Counter(document))[unique_words[i]]   #Calculate the term frequency
             ni = matrix[unique_words[i]]    #Calculate number of times ith word occurs
             tfidf = tf * (N/ni)     # Calculate the term frequency - inverse document frequency of the ith word
             output[i] = tfidf   #Insert the tf-idf value in its respective index in the vector
-    
     return output 
 
 
-def create_word_vector(input_filename):
+def create_word_vector(questions_filename, topics_filename):
     """ Takes a filename as input.
     Returns the the number of documents in the file, a list of all documents and a counter all uniques words.
     """
     N = 0
     documents = []
+    topics = []
     unique_words = []
     noise = " \"+*@#$%&^.,!?;:/()<>|[]"     #All possible pi=unctuations and noise 
     try:
-        inputfile = open(input_filename, 'r')
-        for doc in (inputfile.readlines()):
+        questionsfile = open(questions_filename, 'r')
+        topics_file = open(topics_filename, 'r')
+        for doc in (questionsfile.readlines()):
             documents.append(doc)
             doc = doc.strip().lower()   #Strip and lowercase all docs in the file
             words = doc.split(' ')      #Split the doc with the space deliminiter to get all words in the doc
@@ -93,10 +80,13 @@ def create_word_vector(input_filename):
                     word = word.replace(mark,'')    #Replace punctuations and noise with space in each word in the doc
             unique_words = unique_words + words
             N = N + 1
-        inputfile.close()
+        questionsfile.close()
+        for topic in (topics_file.readlines()):
+            topics.append(topic)
+        topics_file.close()
     except IOError:
         logging.exception('')
-    return N, documents, Counter(unique_words)
+    return N, documents, topics, Counter(unique_words)
 
 
 def cosine_similarity(a, b):
@@ -110,4 +100,4 @@ def cosine_similarity(a, b):
 
 
 if __name__ == "__main__":
-    print(k_nearest_neighbour(" Can you write a smart contract for \"Hello Word\" in solidity?", 10, questions_file_name, topics_file_name))
+    print(test(documents_file, 10, questions_file_name, topics_file_name))
